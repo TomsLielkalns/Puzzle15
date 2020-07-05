9ba718c4576d1eee3b6cf235fcad4477005b8fc9
TomsLielkalns <tomslielkalns@gmail.com>
2020-07-05 16:59:32 +0300

    logic works

--- PuzzleArea.cs
+++ PuzzleArea.cs
@@ -32,22 +32,67 @@ namespace Puzzle15
             PuzzleBlock block;
             for(int row = 1; row < 5; row++)
             {
-                for(int col = 1; col < 5; col++)
+                for (int col = 1; col < 5; col++)
                 {
-                    block = new PuzzleBlock();
-                    block.Top = row * 84;
-                    block.Left = col * 84;
-                    block.Text = blockCount.ToString();
-                    if(blockCount == 16)
+                    block = new PuzzleBlock()
                     {
+                        Top = row * 84,
+                        Left = col * 84,
+                        Text = blockCount.ToString(),
+                        Name = "Block" + blockCount.ToString()
+                    };
+
+                    //block.Click += new EventHandler(Block_Click);
+                    block.Click += Block_Click;
+
+                    if (blockCount == 16)
+                    {
+                        block.Name = "EmptyBlock";
                         block.Text = string.Empty;
-                        block.BackColor = Color.LightGray;
+                        block.BackColor = Color.SkyBlue;
                         block.FlatStyle = FlatStyle.Flat;
                         block.FlatAppearance.BorderSize = 0;
                     }
                     blockCount++;
                     this.Controls.Add(block);
                 }
+            }
+        }
+
+        private void Block_Click(object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            if (CheckIfAdjacent(block))
+            {
+                SwapBlock(block);
+            }
+        }
+
+        private void SwapBlock(Button block)
+        {
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = emptyBlock.Location;
+            emptyBlock.Location = oldLocation;
+        }
+
+        private bool CheckIfAdjacent(Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+
+            a = Math.Abs(emptyBlock.Top - block.Top);
+            b = Math.Abs(emptyBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if(c < 85)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
